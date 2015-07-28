4b364401415a40a5c0b8311ab204e541c88be5d8
Greg Palen <codingvirtual@gmail.com>
2015-07-28 09:37:39 -0500

    FIX: missing document. on lines 92 & 106 TWEAK: simplify call to done in Initial Entries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -29,7 +29,7 @@ $(function () {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /*  Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -46,7 +46,7 @@ $(function () {
             }
         });
 
-        /* TODO: Write a test that loops through each feed
+        /*  Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -64,7 +64,7 @@ $(function () {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /*  Write a new test suite named "The menu" */
     describe('The menu', function () {
         /* This test suite verifies proper functionality of the menu navigation
          * system under the "hamburger" icon in the upper-left corner
@@ -79,7 +79,7 @@ $(function () {
         // the target class we're searching for in some of the tests below
         var targetClass = 'menu-hidden';
 
-        /* TODO: Write a test that ensures the menu element is
+        /*  Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -89,10 +89,10 @@ $(function () {
             // The menu is shown and hidden by manipulating the CSS
             // class of the body element. If the body contains the target
             // class, the menu is hidden.
-            expect(body.classList.contains('menu-hidden')).toBeTruthy();
+            expect(document.body.classList.contains('menu-hidden')).toBeTruthy();
         });
 
-        /* TODO: Write a test that ensures the menu changes
+        /*  Write a test that ensures the menu changes
          * visibility when the menu icon is clicked. This test
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
@@ -103,7 +103,7 @@ $(function () {
             menuIcon.click();
 
             // As before, the document body should not contain the target class
-            expect(body.classList.contains('menu-hidden')).toBeFalsy();
+            expect(document.body.classList.contains('menu-hidden')).toBeFalsy();
 
             // Click the menu icon again. Now it should disappear
             menuIcon.click();
@@ -116,10 +116,10 @@ $(function () {
 
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /*  Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function () {
 
-        /* TODO: Write a test that ensures when the loadFeed
+        /*  Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test wil require
@@ -131,9 +131,7 @@ $(function () {
         // Jasmine's done() function.
         //
         beforeEach(function (done) {
-            loadFeed(0, function () {
-                done();
-            });
+            loadFeed(0, done);
         });
 
         // Since beforeEach will have already run, meaning loadFeed has
@@ -144,10 +142,10 @@ $(function () {
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /*  Write a new test suite named "New Feed Selection" */
 
     describe('New Feed Selection', function () {
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /*  Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
